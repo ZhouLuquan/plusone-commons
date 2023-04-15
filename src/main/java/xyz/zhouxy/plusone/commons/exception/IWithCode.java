1ed01efabc0772bf5e46a34a4f11e2daf03bbf17
ZhouXY108 <luquanlion@outlook.com>
2023-04-16 00:43:34 +0800

    将 IWithCode 和 BaseException 移回来。

---
+++ src/main/java/xyz/zhouxy/plusone/commons/exception/IWithCode.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2022-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package xyz.zhouxy.plusone.commons.exception;
+
+/**
+ * 规定实现类带有 {@code getCode} 方法。
+ * 用于像自定义异常等需要带有 {@code code} 字段的类，
+ * 方便其它地方的程序判断该类的是否实现了此接口，以此获取其实例的 {@code code} 字段的值。
+ *
+ * @author <a href="https://gitee.com/zhouxy108">ZhouXY</a>
+ * @see BaseException
+ */
+public interface IWithCode {
+    int getCode();
+}
