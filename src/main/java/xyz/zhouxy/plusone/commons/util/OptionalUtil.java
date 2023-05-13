14d7a53147935e9ab1f7646ab3fbc4eab333150c
ZhouXY108 <luquanlion@outlook.com>
2023-05-13 13:16:14 +0800

    添加注释。

--- src/main/java/xyz/zhouxy/plusone/commons/util/OptionalUtil.java
+++ src/main/java/xyz/zhouxy/plusone/commons/util/OptionalUtil.java
@@ -38,26 +38,83 @@ import javax.annotation.Nullable;
  */
 public class OptionalUtil {
 
+    /**
+     * 将包装类 {@link Integer} 转为 {@link OptionalInt}（not null）。
+     * <p>
+     * 包装类为 {@code null} 表示值的缺失，转为 {@link OptionalInt} 后，由
+     * {@link OptionalInt#empty()} 表示值的缺失。
+     * </p>
+     * 
+     * @param value 包装对象
+     * @return {@link OptionalInt} 实例
+     */
     public static OptionalInt optionalOf(@Nullable Integer value) {
         return value != null ? OptionalInt.of(value) : OptionalInt.empty();
     }
 
+    /**
+     * 将 {@code Optional<Integer>} 转为 {@link OptionalInt}。
+     * <p>
+     * {@code Optional<Integer>} 将整数包装了两次，改为使用 {@link OptionalInt} 包装其中的整数数据。
+     * </p>
+     * 
+     * @param value 包装对象
+     * @return {@link OptionalInt} 实例
+     */
     public static OptionalInt toOptionalInt(Optional<Integer> objectOptional) {
         return optionalOf(objectOptional.orElse(null));
     }
 
+    /**
+     * 将包装类 {@link Long} 转为 {@link OptionalLong}（not null）。
+     * <p>
+     * 包装类为 {@code null} 表示值的缺失，转为 {@link OptionalLong} 后，由
+     * {@link OptionalLong#empty()} 表示值的缺失。
+     * </p>
+     * 
+     * @param value 包装对象
+     * @return {@link OptionalLong} 实例
+     */
     public static OptionalLong optionalOf(@Nullable Long value) {
         return value != null ? OptionalLong.of(value) : OptionalLong.empty();
     }
 
+    /**
+     * 将 {@code Optional<Long>} 转为 {@link OptionalLong}。
+     * <p>
+     * {@code Optional<Long>} 将整数包装了两次，改为使用 {@link OptionalLong} 包装其中的整数数据。
+     * </p>
+     * 
+     * @param value 包装对象
+     * @return {@link OptionalLong} 实例
+     */
     public static OptionalLong toOptionalLong(Optional<Long> objectOptional) {
         return optionalOf(objectOptional.orElse(null));
     }
 
+    /**
+     * 将包装类 {@link Double} 转为 {@link OptionalDouble}（not null）。
+     * <p>
+     * 包装类为 {@code null} 表示值的缺失，转为 {@link OptionalDouble} 后，由
+     * {@link OptionalDouble#empty()} 表示值的缺失。
+     * </p>
+     * 
+     * @param value 包装对象
+     * @return {@link OptionalDouble} 实例
+     */
     public static OptionalDouble optionalOf(@Nullable Double value) {
         return value != null ? OptionalDouble.of(value) : OptionalDouble.empty();
     }
 
+    /**
+     * 将 {@code Optional<Double>} 转为 {@link OptionalDouble}。
+     * <p>
+     * {@code Optional<Double>} 将整数包装了两次，改为使用 {@link OptionalDouble} 包装其中的整数数据。
+     * </p>
+     * 
+     * @param value 包装对象
+     * @return {@link OptionalDouble} 实例
+     */
     public static OptionalDouble toOptionalDouble(Optional<Double> objectOptional) {
         return optionalOf(objectOptional.orElse(null));
     }
