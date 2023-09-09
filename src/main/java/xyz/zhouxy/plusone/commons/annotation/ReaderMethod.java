85c6bf012c8eb7ab7d2cc52ac8283c73dc5a4664
ZhouXY108 <luquanlion@outlook.com>
2023-09-09 11:08:25 +0800

    添加 ReaderMethod 和 WriterMethod 注解，用于标识方法是读是写。

---
+++ src/main/java/xyz/zhouxy/plusone/commons/annotation/ReaderMethod.java
@@ -0,0 +1,12 @@
+package xyz.zhouxy.plusone.commons.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// TODO 添加 Javadoc
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ReaderMethod {
+}
