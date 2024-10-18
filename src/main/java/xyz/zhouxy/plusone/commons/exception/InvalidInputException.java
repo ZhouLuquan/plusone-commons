cfc1ddc6b3f00ebfb3f6333f181e9c929e64bd86
ZhouXY108 <luquanlion@outlook.com>
2024-10-18 18:31:40 +0800

    新增 InvalidInputException

---
+++ src/main/java/xyz/zhouxy/plusone/commons/exception/InvalidInputException.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package xyz.zhouxy.plusone.commons.exception;
+
+/**
+ * 输入参数无效异常
+ * 
+ * @author ZhouXY
+ */
+public class InvalidInputException extends RuntimeException {
+    private static final long serialVersionUID = 20241017111423L;
+
+    public InvalidInputException() {
+        super();
+    }
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+
+    public InvalidInputException(Throwable cause) {
+        super(cause);
+    }
+
+    public InvalidInputException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
