274f2d987467f9a5a4bcbaf70a373cdf524d122c
ZhouXY108 <luquanlion@outlook.com>
2024-12-02 18:29:34 +0800

    修改 ParsingFailureException 的注释

--- src/main/java/xyz/zhouxy/plusone/commons/exception/ParsingFailureException.java
+++ src/main/java/xyz/zhouxy/plusone/commons/exception/ParsingFailureException.java
@@ -18,12 +18,14 @@ package xyz.zhouxy.plusone.commons.exception;
 
 import java.time.format.DateTimeParseException;
 
+import xyz.zhouxy.plusone.commons.exception.business.RequestParamsException;
+
 /**
  * 解析失败异常
  *
  * <p>
- * 解析失败的不一定是客户传的参数，也可能是其它来源的数据解析失败
- * 如果表示用户传参造成的解析失败，可使用 RequestParamsException(Throwable cause)，
+ * 解析失败的不一定是客户传的参数，也可能是其它来源的数据解析失败。
+ * 如果表示用户传参造成的解析失败，可使用 {@link RequestParamsException#RequestParamsException(Throwable)}，
  * 将 ParsingFailureException 包装成 {@link RequestParamsException} 再抛出
  * </p>
  *
