4e6028f2172ca640f75ca6ea17f04ff803fe80ec
ZhouXY108 <luquanlion@outlook.com>
2025-03-28 11:49:58 +0800

    docs: 介绍 UnifiedResponses 的文档中 CustomUnifiedResponseFactoryTests 链接到代码托管平台对应的页面
    close plusone/plusone-commons#33

--- src/main/java/xyz/zhouxy/plusone/commons/model/dto/package-info.java
+++ src/main/java/xyz/zhouxy/plusone/commons/model/dto/package-info.java
@@ -55,8 +55,9 @@
  * <p>
  * 可使用 {@link UnifiedResponses} 快速构建 {@link UnifiedResponse} 对象。
  * {@link UnifiedResponses} 默认的成功代码为 "2000000"，
- * 用户按测试类 CustomUnifiedResponseFactoryTests 中所示范的，继承 {@link UnifiedResponses}
- * 实现自己的工厂类，
+ * 用户按测试类
+ * <a href="http://zhouxy.xyz:3000/plusone/plusone-commons/src/branch/main/src/test/java/xyz/zhouxy/plusone/commons/model/dto/CustomUnifiedResponseFactoryTests.java">CustomUnifiedResponseFactoryTests</a>
+ * 中所示范的，继承 {@link UnifiedResponses} 实现自己的工厂类，
  * 自定义 SUCCESS_CODE 和 DEFAULT_SUCCESS_MSG 和工厂方法。
  * 见 <a href="http://zhouxy.xyz:3000/plusone/plusone-commons/issues/22">issue#22</a>。
  * </p>
